33dfacf185d080a555ffd179658491b6973458eb
Alex Conder <alex.conder@nerdweb.com.br>
2022-06-03 08:57:14 -0300

    feat: creating dashboard

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+import { Container } from '../../style/global'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default Dashboard
