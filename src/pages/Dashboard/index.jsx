11c6fa364db0c34c23c28b2eb1337644048f97ad
Alex Conder <alex.conder@nerdweb.com.br>
2022-06-06 17:53:42 -0300

    feat: login/logout function

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Header from '../../components/Header'
-import { Container } from '../../style/global'
+import React from "react";
+import Header from "../../components/Header";
+import { Container } from "../../style/global";
+import DashboardForm from "./DashboardForm";
+import NoteList from "./NoteList";
+import { StyledMain } from "./style";
 
-const Dashboard = () => {
+const Dashboard = ({ addNotes, setLogin }) => {
   return (
-    <div>
-        <Header />
-    </div>
-  )
-}
+    <>
+      <Header setLogin={setLogin} />
+      <Container>
+        <StyledMain>
+          <DashboardForm />
+          <NoteList />
+        </StyledMain>
+      </Container>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
